14df7cd02ced3ed8dd7d8fea2f6f86b1c89d4d18
m-rashid <mustafa.rashidZA@gmail.com>
2017-05-14 10:18:14 +0200

    Define iterator inner class

--- Image.h
+++ Image.h
@@ -30,7 +30,51 @@ namespace  RSHMUS001 {
         friend std::ostream & operator << (std::ostream & os, const Image & img);   //input assignment
         friend std::istream & operator >> (std::istream & is, Image & img);         //output assignment
 
-        
+        class iterator {
+
+        private:
+            unsigned char *ptr;
+            friend class Image;
+            iterator (unsigned char *p) : ptr(p) {};    //construct only via Image class (begin/end)
+
+        public:
+            iterator (const iterator & rhs) : ptr(rhs.ptr) {};  //copy constructor
+
+            /* appropriate iterator operator overloading */
+            iterator & operator = (const iterator & rhs) {  //copy assignment
+                if (this != &rhs){
+                    ptr = rhs.ptr;
+                }
+                return *this;
+            }
+
+            iterator & operator ++ (void) {                 //increment
+                ptr += 1;
+                return *this;
+            }
+
+            iterator & operator -- (void) {                 //decrement
+                ptr -= 1;
+                return *this;
+            }
+
+            iterator & operator * (void) {                  //dereference
+                return *ptr;
+            }
+
+            bool operator == (const iterator & rhs) {       //equality
+                return ptr == rhs.ptr;
+            }
+
+            bool operator != (const iterator & rhs) {       //inequality
+                return ptr != rhs.ptr;
+            }
+
+        };
+
+        iterator begin (void) const;
+        iterator end (void) const;
+
     };
 
 }
