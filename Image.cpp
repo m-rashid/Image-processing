b73d0e12bf14d4c2e114304af84e61c57152755b
m-rashid <mustafa.rashidZA@gmail.com>
2017-05-14 12:21:06 +0200

    Define constructors and destructor for Image class

---
+++ Image.cpp
@@ -0,0 +1,59 @@
+#include "Image.h"
+
+using namespace std;
+
+namespace RSHMUS001 {
+
+    Image::Image () {
+
+        width = 0;
+        height = 0;
+        data = nullptr;
+
+    }
+
+    Image::~Image () {
+
+        data = nullptr;
+        width = 0;
+        height = 0;
+
+    }
+
+    Image::Image (const Image & rhs) : width(rhs.width), height(rhs.height), data(new unsigned char(*rhs.data)) {}
+
+    Image::Image (Image && rhs) : width(rhs.width), height(rhs.height), data(move(rhs.data)) {
+
+        data = nullptr;
+
+    }
+
+    Image & Image::operator = (const Image & rhs) {
+
+        if(this != rhs) {
+            width = rhs.width;
+            height = rhs.height;
+            data.reset(new int (*rhs.data));
+        }
+        return *this;
+
+    }
+
+    Image & Image::operator = (Image && rhs) {
+
+        if(this != rhs) {
+            width = rhs.width;
+            hieght = rhs.height;
+            data = move(rhs.data);
+        }
+
+        return *this;
+    }
+
+    Image & operator + (const Image & rhs) {
+
+
+        
+
+    }
+}
